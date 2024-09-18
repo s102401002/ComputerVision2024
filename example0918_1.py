be10406c7c664fffe29ca6f76f37dd46d2995c93
easonwang <s102401002@gmail.com>
2024-09-18 15:29:00 +0800

    trackbar2

--- example0918_1.py
+++ example0918_1.py
@@ -6,20 +6,21 @@ def empty(v):
     pass
 
 image = cv2.imread('img/lena.bmp', cv2.IMREAD_GRAYSCALE)
+
 if image is None:
     print("Error: Image not found.")
     sys.exit()
+cv2.namedWindow('result',cv2.WINDOW_NORMAL)
 
 cv2.namedWindow('trackBar')
-cv2.resizeWindow('trackBar', 320, 320)
-
 cv2.createTrackbar('window_size', 'trackBar', 15, 200, empty)
-cv2.createTrackbar('const', 'trackBar', 2, 200, empty)
+cv2.createTrackbar('const', 'trackBar', 2, 40, empty)
 
 while True:
     window_size = cv2.getTrackbarPos('window_size', 'trackBar')
     const = cv2.getTrackbarPos('const', 'trackBar')
-
+    if window_size <= 1: 
+        window_size = 3
     if window_size % 2 == 0:
         window_size += 1
 
@@ -38,7 +39,8 @@ while True:
     # Show the result
     cv2.imshow("result", binary_image)
 
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # Clean up
 cv2.destroyAllWindows()
